02f34e88cc2f73910134d4832709819fcde58b8c
brian_ogola <brianochiengogola78@gmail.com>
2023-03-08 21:06:03 +0300

    farmfresh

--- resources/views/Components/admin-master.blade.php
+++ resources/views/Components/admin-master.blade.php
@@ -12,7 +12,7 @@
   <title>SB Admin 2 - Dashboard</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="{{asset('admin_ui/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -59,7 +59,7 @@
       <x-admin-crop></x-admin-crop>
 
       <!-- Nav Item - Animal Collapse Menu -->
-      
+
       <x-admin-animal></x-admin-animal>
 
       <!-- Divider -->
@@ -312,7 +312,7 @@
         @yield('content')
 
 
-        
+
       <!-- Footer -->
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
