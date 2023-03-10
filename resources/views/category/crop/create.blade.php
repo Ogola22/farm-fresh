86c9e12719c5e572ef08c9f5a54fb2cdf096fb27
brian_ogola <brianochiengogola78@gmail.com>
2023-03-10 13:27:16 +0300

    farmfresh

--- resources/views/category/crop/create.blade.php
+++ resources/views/category/crop/create.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Untitled</title>
+    <title>add crops</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -19,8 +19,8 @@
             <form method="POST" action="{{route('crop.store')}}" class="contact100-form validate-form">
                 @csrf
                 <span class="contact100-form-title">Enter Crop Data</span>
-                
-                <label for="name" class="contact100-form-label">{{_('name')}}</label>
+
+                <label for="name" class="contact100-form-label">{{_('Name')}}</label>
                 <div class="wrap-input100 validate-input" data-validate="Please enter your name">
                     <input class="form-control input100 @error('name') is-invalid @enderror" id="name" type="text" name="name" placeholder="Enter crop name" autocomplete="name" autofocus required>
                     @error('name')
@@ -30,7 +30,7 @@
                     @enderror
                 </div>
 
-                <label for="crop_duration" class="contact100-form-label">{{_('crop_duration')}}</label>
+                <label for="crop_duration" class="contact100-form-label">{{_('Crop Duration')}}</label>
                 <div class="wrap-input100 validate-input " data-validate="Crop Duration in Number of Days">
                     <input class="form-control input100 @error('crop_duration') is-invalid @enderror" id="crop_duration" type="number" name="crop_duration" placeholder="Crop Duration in Number of Days" autocomplete="crop_duration" autofocus required>
                     @error('crop_duration')
@@ -40,7 +40,7 @@
                     @enderror
                 </div>
 
-                <label for="farmers_note" class="contact100-form-label">{{_('farmers_note')}}</label>
+                <label for="farmers_note" class="contact100-form-label">{{_('Farmers Note')}}</label>
                 <div class="wrap-input100 validate-input" data-validate="Please enter a farmers note about your crop">
                     <textarea class="form-control input100" name="farmers_note" placeholder="Your farmers_note" row="10"></textarea>
                     <span class="focus-input100"></span>
